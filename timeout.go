a3475b3bdcfd50c4e9c18fae883d9ed43a280e00
chrise <chris.every@paxport.net>
2017-10-06 00:10:28 +0100

    use millisecs for calculating response time

--- timeout.go
+++ timeout.go
@@ -8,14 +8,14 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  secs := time.Now().Unix()
+  millisecs := float64(time.Now().UnixNano())/float64(1000000)
   urlPathString := r.URL.Path[1:]
   urlPathInt, err := strconv.ParseInt(urlPathString, 10, 0)
 
   if err == nil{
     duration := time.Duration(urlPathInt)*time.Second
     time.Sleep(duration)
-    fmt.Fprintf(w, "app resp time: %ds", time.Now().Unix() - secs)
+    fmt.Fprintf(w, "app resp time: %fs", (float64(time.Now().UnixNano())/float64(1000000) - millisecs)/1000)
   }else{
     fmt.Fprintf(w, "you didn't specify an int as the pause value, silly..")
   }
