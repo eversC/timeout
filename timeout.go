034d2a9a43e9400f92eed2a9d8f21f480a2926fe
chrise <chris.every@paxport.net>
2017-10-06 08:50:48 +0100

    stop a stupidly large pause value being requested. I don't really see the point in anyone actually wanting to wait more than a day for a resp...

--- timeout.go
+++ timeout.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "time"
-    "strconv"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  millisecs := float64(time.Now().UnixNano())/float64(1000000)
-  urlPathString := r.URL.Path[1:]
-  urlPathInt, err := strconv.ParseInt(urlPathString, 10, 0)
+	millisecs := float64(time.Now().UnixNano()) / float64(1000000)
+	urlPathString := r.URL.Path[1:]
+	urlPathInt, err := strconv.ParseInt(urlPathString, 10, 0)
 
-  if err == nil{
-    duration := time.Duration(urlPathInt)*time.Second
-    time.Sleep(duration)
-    fmt.Fprintf(w, "app resp time: %fs", (float64(time.Now().UnixNano())/float64(1000000) - millisecs)/1000)
-  }else{
-    fmt.Fprintf(w, "you didn't specify an int as the pause value, silly..")
-  }
+	if err == nil {
+		if urlPathInt > 86400 {
+			fmt.Fprint(w, "woah, do you REALLY want to wait that long?")
+		} else {
+			duration := time.Duration(urlPathInt) * time.Second
+			time.Sleep(duration)
+			fmt.Fprintf(w, "app resp time: %fs",
+				(float64(time.Now().UnixNano())/float64(1000000)-millisecs)/1000)
+		}
+	} else {
+		fmt.Fprintf(w, "you didn't specify an int as the pause value, silly..")
+	}
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
